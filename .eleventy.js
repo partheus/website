e6cca47d4f2ab149c260537b2441af97ed6df3b2
Parth <parth.shiralkar@gmail.com>
2020-05-26 21:08:15 -0500

    updated templates

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     return {
         passthroughFileCopy: true,
+        markdownTemplateEngine: "njk",
+        templateFormats: ["njk","html","md"], 
         dir: {
             input: "src",
             output: "_site",

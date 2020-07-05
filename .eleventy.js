9d794abbc219da8b0b15747fb694473f798dfc85
Parth <parth.shiralkar@gmail.com>
2020-07-05 11:57:51 -0500

    Site updates, add new blog series

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,7 @@ module.exports = function(eleventyConfig) {
   
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("icons");
+    eleventyConfig.addPassthroughCopy("img");
     return {
       passthroughFileCopy: true
     }
